f6c20c92b438da90f5c1fdf11976f13885c325c6
tosxin-456 <tosinekshally@gmail.com>
2024-05-21 12:40:39 +0100

    added the animation

--- src/components/highlight.tsx
+++ src/components/highlight.tsx
@@ -1,35 +1,63 @@
-// import React from 'react';
-import { Box } from '@chakra-ui/react'; // Replace with your actual Box component import
+import React, { useEffect, useRef } from 'react';
+import { Box, Text } from '@chakra-ui/react';
+import Typed from 'typed.js';
 
 interface FooterProps {
-  isLightMode: boolean; // Example type definition
+  isLightMode: boolean;
 }
 
-function Highlight({isLightMode}:FooterProps) {
-  
-  // JSX structure with <ul> and <li> elements
+function Highlight({ isLightMode }: FooterProps) {
+  const el = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    if (el.current) {
+      const typed = new Typed(el.current, {
+        strings: [
+          `function <b>love</b>()  {
+            <br>
+          <b>life.</b>happiness += 10;
+          <br>
+          console.log("Love is in the air! Happiness level: " + life.happiness);
+           <br>
+          }
+          <br>
+          function lifeEvents(event) {
+            <br>
+          if (event === "love") {
+          <br>
+          love();
+          <br>
+          } else {
+            <br>
+          console.log("Life is okay, but happiness level remains: " + life.happiness);
+          <br>
+          }
+          <br>
+          }
+          <br>
+          <b>love();</b>`
+        ],
+        typeSpeed: 35,
+        backSpeed: 500,
+        backDelay: 30,
+        loop: true,
+      });
+
+      return () => {
+        typed.destroy();
+      };
+    }
+  }, []);
+
   return (
     <Box
-    className={`${isLightMode ?'bg-[white] text-[#3b5994] shadow-lg' :'bg-[#141613]'}  m-auto mt-[20px] text-[15px] lg:ml-auto lg:mt-[150px] lg:mr-[10%] p-[15px] w-[80%] lg:w-[400px] rounded-[7px] text-start `}
-    // width={'400px'}
-    height={'fit-content'}
-    color={'#6A98F0'}
+      className={`${isLightMode ? 'bg-[white] text-[#3b5994] shadow-lg' : 'bg-[#141613]'} m-auto mt-[20px] text-[15px] lg:ml-auto lg:mt-[150px] lg:mr-[10%] p-[15px] w-[80%] lg:w-[400px] rounded-[7px] text-start`}
+      height={'fit-content'}
+      color={'#6A98F0'}
     >
-      <ul className= {`${isLightMode ? 'text-[#3b5994]':'text-[#6A98F0]'}`} >
-        <li>function <b>love</b>()  &#123;</li>
-        <li> <b>life.</b>happiness += 10;</li>
-        <li>console.log("Love is in the air! Happiness level: " + life.happiness);</li>
-        <li>&#125;</li>
-
-        <li>function lifeEvents(event) &#123;</li>
-        <li>if (event === "love") &#123;</li>
-        <li>love();</li>
-        <li>&#125; else &#123;</li>
-        <li>console.log("Life is okay, but happiness level remains: " + life.happiness);</li>
-        <li>&#125;</li>
-        
-        <li><b>love();</b></li>
-      </ul>
+      <Text as="div" display="inline-block" className={`${isLightMode ? 'text-[#3b5994]' : 'text-[#6A98F0]'}`}>
+        <span ref={el}></span>
+      </Text>
     </Box>
   );
 }
--- src/components/topskills.tsx
+++ src/components/topskills.tsx
@@ -1,8 +1,31 @@
 // import React from 'react'
 import { Text, VStack,} from '@chakra-ui/react';
-
+// import { useEffect, useRef } from 'react';
+// import Typed from 'typed.js'
 
 function Topskills() {
+    // const el = useRef<HTMLDivElement | null>(null);
+  
+    // useEffect(() => {
+    //   if (el.current) {
+    //     const typed = new Typed(el.current, {
+    //       strings: ['Events', 'Locations', 'Business'],
+    //       typeSpeed: 100,
+    //       backSpeed: 50,
+    //       backDelay: 3000,
+    //       loop: true, // This should be a boolean
+    //     });
+  
+    //     // Cleanup to destroy the Typed instance when the component unmounts
+    //     return () => {
+    //       typed.destroy();
+    //     };
+    //   }
+    // }, []);
+  
+
+
+
   return (
     <VStack
     mt={'35px'}
@@ -16,6 +39,11 @@ function Topskills() {
       >
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam quibusdam ratione officia eius exercitationem molestiae. Similique ipsa fugit nisi ipsam enim, iste ea neque laboriosam! Qui pariatur aut laboriosam, odio dolores iste a harum maxime architecto ut voluptatem suscipit excepturi tempore quod accusamus odit soluta vitae! Temporibus obcaecati hic ab.
       </Text>
+      {/* <Text as="div" display="inline-block">
+        <span ref={el}></span>
+      </Text> */}
+          
+
     </VStack>
   )
 }
