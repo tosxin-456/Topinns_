05a7a39619f8f449739f538898b1aad04c8b4af1
tosxin-456 <tosinekshally@gmail.com>
2024-05-21 22:49:17 +0100

    made the light and dark themes permanant

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,34 @@ import Home from './pages/home'
 
 
 function App() {
-  const [isLightMode, setIsLightMode] = useState(true);
+  const [isLightMode, setIsLightMode] = useState(() => {
+    const theme = localStorage.getItem('mode');
+    return theme ? theme === 'light' : true; // Default to light mode if no preference is set
+  });
 
   useEffect(() => {
     if (isLightMode) {
+      localStorage.setItem('mode', 'light');
       document.body.style.backgroundColor = 'white';
       document.body.style.color = 'black';
     } else {
+      localStorage.setItem('mode', 'dark');
       document.body.style.backgroundColor = '#0B0E13';
       document.body.style.color = 'white';
     }
   }, [isLightMode]);
+
+  useEffect(() => {
+    const theme = localStorage.getItem('mode');
+    if (theme === 'light') {
+      document.body.style.backgroundColor = 'white';
+      document.body.style.color = 'black';
+    } else if (theme === 'dark') {
+      document.body.style.backgroundColor = '#0B0E13';
+      document.body.style.color = 'white';
+    }
+  }, []);
+
 
   const toggleColorMode = () => {
     setIsLightMode(prevMode => !prevMode);
