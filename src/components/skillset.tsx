e5568e9d2a1239a544124ca16257ae686c4064a8
tosxin-456 <tosinekshally@gmail.com>
2024-05-22 20:24:50 +0100

    added the skillset with icons

--- src/components/skillset.tsx
+++ src/components/skillset.tsx
@@ -1,6 +1,11 @@
-import { Flex , VStack, Box, Text } from '@chakra-ui/react'
+import { Flex , VStack, Box, Text , Image } from '@chakra-ui/react'
 // import React from 'react'
-
+// import { FaConnectdevelop } from "react-icons/fa";
+// import { SiBackendless } from "react-icons/si";
+import { SiNestjs } from "react-icons/si";
+import { FaReact } from "react-icons/fa";
+import { FaNode } from "react-icons/fa";
+import { SiGoogletagmanager } from "react-icons/si";
 
 interface SkillsProps {
     isLightMode: boolean; // Assuming this prop is passed to Home component
@@ -11,46 +16,73 @@ function Skillset({isLightMode}:SkillsProps) {
   return (
     <VStack>
         <Text className='text-3xl'>My SkillSet</Text>
-        <Box
-        width={'50%'}
-        // className='shadow-lg p-[10px] rounded-lg'
-        className={`${isLightMode ?'bg-[white] text-black shadow-lg p-[15px] rounded-lg' :'bg-[#191A26] text-white p-[10px]  rounded-lg'}`}
-        >
-        <Text  className='text-[17px] text-center'>
-           Front-end Development Expertise
-        </Text>
-        <Text fontFamily='"Poppins", "sans-serif"' className='text-[13px] text-center'>
-        Crafting dynamic interfaces, I build fast, scalable, and accessible web applications.
-        </Text>
-        </Box>
       <Flex>
         <Box
-        width={'45%'}
+        width={'47%'}
         marginRight={'auto'}
+        marginLeft={'20px'}
         // className='shadow-lg p-[10px] rounded-lg'
-        className={`${isLightMode ?'bg-[white] text-black shadow-lg p-[10px] rounded-lg' :'bg-[#191A26] text-white p-[7px]  rounded-lg'}`}
+        className={`${isLightMode ?'bg-[white] text-black shadow-lg p-[10px] rounded-lg' :'bg-[#191A26] text-white p-[15px]  rounded-lg'}`}
         >
-        <Text className='text-[17px] text-center'>
+        <Box
+        margin={'auto'}
+        width={'fit-content'}
+        display={'flex'}
+        >
+        <Text width={'fit-content'} className='text-[18px] text-center flex'>
+        <SiNestjs className='w-[50px] h-[30px] mr-auto' />
            Back-end Development Expertise
         </Text>
-        <Text fontFamily='"Poppins", "sans-serif"' className='text-[13px] text-center'>
+        </Box>    
+        <Text fontFamily='"Poppins", "sans-serif"' className='text-[15px] text-center'>
         Building robust and scalable server-side logic, I create efficient APIs and seamless user experiences
         </Text>
         </Box>
         <Box
-        width={'45%'}
+        width={'47%'}
         marginLeft={'auto'}
+        marginRight={'20px'}
+        height={'150px'}
         // className='shadow-lg p-[10px] rounded-lg'
-        className={`${isLightMode ?'bg-[white] text-black shadow-lg p-[10px] rounded-lg' :'bg-[#191A26] text-white p-[7px]  rounded-lg'}`}
+        className={`${isLightMode ?'bg-[white] text-black shadow-lg p-[10px] rounded-lg' :'bg-[#191A26] text-white p-[15px]  rounded-lg'}`}
         >
-        <Text  className='text-[17px] text-center'>
-           Management and Development
+            <Box
+            margin={'auto'}
+            width={'fit-content'}
+            display={'flex'}
+            height={'fit-content'}
+            >
+          <FaReact className='w-[50px] h-[30px] mr-auto' />
+        <Text  className='text-[18px] text-center'>
+        Front-end Development Expertise
         </Text>
-        <Text fontFamily='"Poppins", "sans-serif"' className='text-[13px] text-center'>
-        I efficiently manage development workflows and collaborate with teams to deliver high-quality projects on time.
+            </Box>
+        <Text fontFamily='"Poppins", "sans-serif"' className='text-[15px] text-center'>
+        Crafting dynamic interfaces, I build fast, scalable, and accessible web applications, with React, Tailwind, etc.
         </Text>
         </Box>
       </Flex>
+      <Box
+        width={'80%'}
+        marginTop={'30px'}
+        // className='shadow-lg p-[10px] rounded-lg'
+        className={`${isLightMode ?'bg-[white] text-black shadow-lg p-[15px] rounded-lg' :'bg-[#191A26] text-white p-[15px]  rounded-lg'}`}
+        >
+        <Box 
+        margin={'auto'}
+        width={'fit-content'}
+        display={'flex'}
+        height={'40px'}
+        >
+        <SiGoogletagmanager className='w-[25px] mr-[5px] mt-[2px]' /> 
+        <Text  className='text-[15px] text-center'>
+          Management and Development
+        </Text>
+            </Box >
+        <Text fontFamily='"Poppins", "sans-serif"' className='text-[15px] text-center'>
+        I efficiently manage development workflows and collaborate with teams to deliver high-quality projects on time.
+        </Text>
+        </Box>
     </VStack>
   )
 }
