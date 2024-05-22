59b634459304c6d0225aa4b802ae747d40d3fb91
tosxin-456 <tosinekshally@gmail.com>
2024-05-22 11:25:59 +0100

    reduced the text

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -12,13 +12,13 @@ function AboutHeader() {
           A Little 'bout {" "} <span className='text-[orange] shadow-sm' >Me</span> 
           </Text>
         <Box className='flex flex-col lg:flex-row' >
-        <Box className='w-[50%] m-auto lg:w-[30%] lg:m-auto '>
+        <Box className='w-[50%] m-auto lg:w-[28%] lg:m-auto '>
        <Image
        borderRadius={'50%'}
-       width={'100%'}
+       width={'90%'}
        margin={'auto'}
        mb={'10px'}
-       mt={'15px'}
+      //  mt={'15px'}
        src={profile} />
         </Box>
         <Box className='w-[100%] lg:w-[70%] '>
@@ -28,7 +28,7 @@ function AboutHeader() {
         textAlign={'start'}
         margin={'auto'}
         mb={'10px'}
-        mt={'15px'}
+        mt={'20px'}
         >
       Hi, I'm Ekundayo Shallom (Topinns), a full-stack developer passionate about creating user-friendly web applications. I trained at <a href="https://nhub-foundation-staging.netlify.app/about" className='text-[#6A98F0] hover:underline' >nHub Foundation </a> and I am currently a front-end developer intern at <a href="https://clocker.databoard.ai" className='text-[#6A98F0] hover:underline' >Databoard Technologies</a> . I work with HTML, CSS, JavaScript, React, Express.js, Nest.js, Tailwind CSS, and Chakra UI. Proficient in Git, GitHub, and Agile methodologies, I focus on optimizing website performance and enhancing user experience. I'm dedicated to delivering high-quality results and continuously growing as a developer.
         </Text>
